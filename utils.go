c88499447f8d92f81bcd32b489bba0370c8b17ff
chenhao392 <chenhao.mymail@gmail.com>
2018-11-16 13:51:47 -0500

    testing different defer location

--- utils.go
+++ utils.go
@@ -24,12 +24,11 @@ func mapCInt(in []int) []C.int {
 
 // convert C double pointer to float64 slice ...
 func doubleToFloats(in *C.double, size int) []float64 {
-	defer C.free(unsafe.Pointer(in))
 	outD := (*[1 << 30]C.double)(unsafe.Pointer(in))[:size:size]
 	out := make([]float64, size, size)
 	for i := 0; i < size; i++ {
 		out[i] = float64(outD[i])
 	}
-
+	defer C.free(unsafe.Pointer(in))
 	return out
 }
